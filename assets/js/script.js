0403c8fc366ec3f1d674597d7d982e40ee479f9f
raffaele-catalano <raf.catalano@gmail.com>
2023-03-17 16:23:36 +0100

    structure prepared + first try

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/*
+chiedere all'utente il nome, l'anno di nascita, e l'età. Il programma restituisce l'anno attuale
+
+1) chiedere e salvare il nome
+2) chiedere e salvare il cognome
+3) chiedere e salvare l'animale preferito
+4) chiedere e salvare il colore preferito
+5) chiedere e salvare un numero compreso tra 0 e 99
+4) salvare l'unione degli ultimi tre elementi
+5) stampare in pagina l'output
+
+*/
+
+const name = prompt('Inserisci il tuo nome');
+const surname = prompt('Inserisci il tuo cognome')
+    console.log(name, surname);
