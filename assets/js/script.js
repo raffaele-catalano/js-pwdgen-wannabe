50436f220802ce6e5f50ad3a6399bfe1a60c6841
raffaele-catalano <raf.catalano@gmail.com>
2023-03-17 16:29:35 +0100

    added five "const" - checked w/ console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,17 @@ chiedere all'utente il nome, l'anno di nascita, e l'età. Il programma restituis
 2) chiedere e salvare il cognome
 3) chiedere e salvare l'animale preferito
 4) chiedere e salvare il colore preferito
-5) chiedere e salvare un numero compreso tra 0 e 99
+5) chiedere e salvare un numero compreso tra 10 e 99
 4) salvare l'unione degli ultimi tre elementi
 5) stampare in pagina l'output
 
 */
 
 const name = prompt('Inserisci il tuo nome');
-const surname = prompt('Inserisci il tuo cognome')
-    console.log(name, surname);
+const surname = prompt('Inserisci il tuo cognome');
+const pet = prompt('Inserisci il tuo animale preferito');
+const color = prompt('Inserisci il tuo colore preferito');
+const number = parseInt(prompt('Scegli un numero compreso tra 10 e 99'));
+    console.log(name, surname, pet, color, number);
+
+
